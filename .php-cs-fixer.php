f62427dfe1df916d4f505fbcc900ac37f6564795
Christer Edvartsen <cogo@starzinger.net>
2026-01-10 22:16:08 +0100

    build: rename and update config

--- .php-cs-fixer.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-require 'vendor/autoload.php';
-
-$finder = (new PhpCsFixer\Finder())
-    ->in(__DIR__)
-    ->append([__FILE__]);
-
-return (new Imbo\CodingStandard\Config())
-    ->setFinder($finder);
